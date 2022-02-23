a7c3bf1a6e72c591c864be3356f80c73d447cb66
Abhishek060201 <abhishektiwari060201@gmail.com>
2022-02-23 13:49:15 +0530

    added some validation using yup

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -2,12 +2,36 @@ import React from 'react';
 import EqualEmployment from '../EqualEmployment/EqualEmployment';
 import Input from '../Input/Input';
 import ReCAPTCHA from "react-google-recaptcha";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import './Form.css';
 
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+const schema = yup.object().shape({
+  fullName: yup.string().min(10).required(),
+  email: yup.string().email().required(),
+  phone: yup.string().matches(phoneRegExp, 'Phone number is not valid'),
+  linkedinURL: yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').required('Please enter website'),
+  twitterURL: yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').required('Please enter website'),
+  githubURL: yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').required('Please enter website'),
+  portfolioURL: yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').required('Please enter website'),
+  otherWebsite: yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').required('Please enter website'),
+})
+
 const Form: React.FC = (): JSX.Element => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const submitForm = (data: object) => {
+    console.log(data);
+  }
+
   return (
     <div className='form-container'>
-      <form className='mx-auto' >
+      <form className='mx-auto' onSubmit={handleSubmit(submitForm)}>
 
         <h4>SUBMIT YOUR APPLICATION</h4>
 
@@ -24,40 +48,64 @@ const Form: React.FC = (): JSX.Element => {
             </div>
           </div>
 
+          <p>{errors.fullName?.message}</p>
           <Input
-            isRequired={true}
+            ref={register}
             label='Full name'
+            name='fullName'
+            isReq={true}
           />
           <Input
-            isRequired={true}
+            ref={register}
             label='Email'
+            name='email'
+            isReq={true}
           />
           <Input
-            isRequired={false}
+            ref={register}
             label='Phone'
+            name='phone'
+            isReq={false}
           />
           <Input
-            isRequired={false}
+            ref={register}
             label='Company'
+            name='company'
+            isReq={false}
           />
         </div>
 
         <h4>LINKS</h4>
         <div className="row my-4">
           <Input
+            ref={register}
             label='Linkedin URL'
+            name='linkedinURL'
+            isReq={false}
           />
           <Input
+            ref={register}
             label='Twitter URL'
+            name='twitterURL'
+            isReq={false}
           />
           <Input
+            ref={register}
             label='GitHub URL'
+            name='githubURL'
+            isReq={false}
           />
           <Input
+            ref={register}
             label='Portfolio URL'
+            name='portfolioURL'
+            isReq={false}
           />
           <Input
+            ref={register}
             label='Other website'
+            name='otherWebsite'
+            isReq={false}
           />
         </div>
 
@@ -84,7 +132,12 @@ const Form: React.FC = (): JSX.Element => {
         </div>
 
         <div className='d-flex'>
-          <button className='submit-button mx-auto'>SUBMIT APPLICATION</button>
+          <button
+            type='submit'
+            className='submit-button mx-auto'
+          >
+            SUBMIT APPLICATION
+          </button>
         </div>
 
       </form>
--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -3,7 +3,9 @@ import './Input.css';
 
 type Props = {
   label: string;
-  isRequired?: boolean;
+  name: string;
+  isReq: boolean;
+  ref?: any
 }
 
 const Input = (props: Props): JSX.Element => {
@@ -11,12 +13,12 @@ const Input = (props: Props): JSX.Element => {
     <div className="row d-flex align-items-center">
       <label className='input-label col-md-3'>
         {props.label}
+        {
+          props.isReq ? <span style={{ color: 'red' }}> *</span> : null 
+        }
       </label>
-      {/* {
-        props.isRequired ? <span className='col-md-0'> *</span> : null
-      } */}
       <div className='col-md-9 my-4 my-md-3'>
-        <input />
+        <input type='text' name={props.name} />
       </div>
     </div>
   );
